9247cf1e0c01c20c857f2e7446e22e86cdea6717
mohammad amiri <mohammad.amiri@itggot.se>
2018-02-11 23:02:14 +0100

    Auth function
    Jag kunde bara göra klart den Authentication funktionen men jag håller på att göra ett sådant sida att alla kan lägga sina medelande i den här sidan.

--- app.rb
+++ app.rb
@@ -1,42 +1,53 @@
 class App < Sinatra::Base
-	enable :session 
 
-	# def set_error(error_message)
-	# 	session[:error] =error_message		
-	# end
+	enable :sessions
 
-	# def get_error()
-	# 	error = session[:error]
-	# 	session[:error] = nil
-	# 	return error
-	# end
+	def set_error(error_message)
+		session[:error] = error_message
+	end
 
-	get '/' do
+	def get_error()
+		error = session[:error]
+		session[:error] = nil
+		return error
+	end
+
+	get('/') do
 		slim(:index)
 	end
 
-	get '/register' do
-		slim :register 
+	get('/register') do
+		slim(:register)
 	end
 
-	post 'register' do 
-		db.SQlite3::Datebase.new('db/Chatsystem.sqlite')
+	get('/error') do
+		slim(:error)
+	end
+
+	get('/notes/create') do
+		slim(:create_note)
+	end
+
+	get '/home' do
+		slim :home
+	end
+
+	post('/register') do
+		db = SQLite3::Database.new('db/Chatsystem.db')
 		db.results_as_hash = true
-
-		username = params["username"]
-		email=params["email"]
-		password = params["password"]
-		password_confirmation = ["confirm_password"]
 		
-		db.execute("INSERT INTO Users(momo) VALUES (?)", [username])
-
-		result = db.execute("SELECT id FROM Users WHERE username=?", [Username] )
+		username = params["username"]
+		email = params["email"]
+		password = params["password"]
+		password_confirmation = params["confirm_password"]
+		
+		result = db.execute("SELECT id FROM users WHERE username=?", [username])
 		
 		if result.empty?
 			if password == password_confirmation
 				password_digest = BCrypt::Password.create(password)
 				
-				db.execute("INSERT INTO Users(1,username,email, password_digest) VALUES (?,?,?,?)", [id,username, email, password_digest])
+				db.execute("INSERT INTO Users(username, password_digest, email) VALUES (?,?,?)", [username, password_digest,email])
 				redirect('/')
 			else
 				set_error("Passwords don't match")
@@ -46,7 +57,36 @@ class App < Sinatra::Base
 			set_error("Username already exists")
 			redirect('/error')
 		end
-		 
-	end 
+		
+	end
+	
+	
+	post('/login') do
+		db = SQLite3::Database.new('db/Chatsystem.db')
+		db.results_as_hash = true
+		username = params["username"]
+		password = params["password"]
+		
+		result = db.execute("SELECT id, password_digest FROM Users WHERE username=?", [username])
 
-end           
+		if result.empty?
+			set_error("Invalid Credentials")
+			redirect('/error')
+		end
+
+		user_id = result.first["id"]
+		password_digest = result.first["password_digest"]
+		if BCrypt::Password.new(password_digest) == password
+			session[:user_id] = user_id
+			redirect('/home')
+		else
+			set_error("Invalid Credentials")
+			redirect('/error')
+		end
+	end
+
+	post('/logout') do
+		session.destroy
+		redirect('/')
+	end
+ end
